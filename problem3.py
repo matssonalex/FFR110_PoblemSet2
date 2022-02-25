407c3572b06d01052cf6ccd40da4d2bc90204fdc
matssonalex <alexmatsson@gmail.com>
2022-02-25 23:07:29 +0100

    update problem3

--- problem3.py
+++ problem3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
 
 
 def circle(vector):
@@ -15,22 +16,27 @@ def circle(vector):
 
 gamma = 0.5
 K_c = 2*gamma
-K = [K_c-1, K_c+0.01, K_c+1]
-K = K[2]
+K = [K_c-0.5, K_c+0.01, K_c+0.5]
+K = K[1]
 N = 300
-theta = np.random.uniform(np.pi/2, -np.pi/2, size=N)
-omega = np.random.standard_cauchy(size=N)
-T = 100
-dt = 0.01
+theta = np.random.uniform(-np.pi/2, +np.pi/2, size=N)
+# omega = np.random.standard_cauchy(size=N)
+omega = stats.cauchy.rvs(loc=0, scale=gamma, size=N)
+T = 200
+dt = 0.1
 r = np.zeros(int(T/dt))
 t = np.linspace(0, T, int(T/dt))
 
 for j in range(int(T/dt)):
-    r[j] = np.sum(np.cos(theta)) / N
+    
     for i in range(N):
         #r[j] = np.abs(np.sum(np.exp(theta)))
-        theta[i] += dt*(omega[i] + K/N*np.sum(np.sin(theta - theta[i])))
 
+        theta[i] += dt*(omega[i] + ((K/N) * np.sum(np.sin(theta - theta[i]))))
+    
+    r[j] = np.linalg.norm([(np.sum(np.cos(theta)) / N), (np.sum(np.sin(theta)) / N)])
 #circle(theta)
-plt.plot(t, np.abs(r))
+# temp = np.sqrt((K - K_c) / K_c)
+# print(temp)
+plt.plot(t, r)
 plt.show()
