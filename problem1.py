755aae3b4e1e7dedc2ce3f18574ee17d8bccdf8c
matssonalex <alexmatsson@gmail.com>
2022-02-25 08:25:10 +0100

    first commit

---
+++ problem1.py
@@ -0,0 +1,127 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.misc import derivative
+import sympy as sym
+plt.rcParams.update({
+    "text.usetex": False,
+    "font.family": "sans-serif",
+    "font.sans-serif": ["Helvetica"],
+    "font.size": 12})
+
+
+def find_steady_states(q_val, rho):
+    u, q, p = sym.symbols('u q p')
+    fp = sym.solve(p*u*(1 - (u/q)) - (u/(1 + u)), u)
+
+    u_star1_val = fp[0].subs([(q, q_val), (p, rho)])
+    u_star2_val = fp[1].subs([(q, q_val), (p, rho)])
+    u_star3_val = fp[2].subs([(q, q_val), (p, rho)])
+    
+    return [u_star1_val, u_star2_val, u_star3_val]
+
+
+def init_ramp_func(xi0, u0, u):
+    for i in range(np.shape(u)[0]):
+        u[i, 0] = u0 / (1 + np.exp((i+1) - xi0))
+
+
+def init_peak_func(xi0, u0, u):
+    for i in range(np.shape(u)[0]):
+        u[i, 0] = u0 * np.exp(-(((i+1) - xi0)**2))
+
+
+# def disc_lap(u, i, tau):
+#     return u[i + 1, tau] + u[i - 1, tau] - 2*u[i, tau]
+
+
+def func(u, i, tau):
+    return rho * u[i, tau] * (1 - (u[i, tau] / q_val)) - (u[i, tau] / (1 + u[i, tau])) + u[i + 1, tau] + u[i - 1, tau] - 2*u[i, tau]
+
+
+def eom(u, i, tau):
+    return u[i, tau] + (dt * func(u, i, tau))
+
+
+def estimate_velocity(u, t):
+    ind = np.where(np.abs(u[24, t] - u[25,:]) < 1e-3)[0][0]
+    v = 1 / (dt * (ind-t))
+    return v
+
+
+def run_dynamics(xi0, u0, init_func):
+    u = np.zeros((L, int(t_max/dt)))
+    
+    if init_func == 1:
+        init_ramp_func(xi0, u0, u)
+    elif init_func == 2:
+        init_peak_func(xi0, u0, u)
+
+    u[0, :] = u[0, 0]
+    u[-1, :] = u[-1, 0] 
+
+    for tau in range(int((t_max/dt)) - 1):
+        for i_xi in range(1, 99):
+            u[i_xi, tau + 1] =  eom(u, i_xi, tau)
+    
+    # take care of boundaries, always same
+        
+
+    return u
+
+
+def b_plot(u, derivative_estimate):
+    t_vec = np.linspace(0, 49999, 10)
+    # # t_vec = [1000, 2000]
+    for t in t_vec:
+        plt.plot(xi, u[:, int(t)])
+    # plt.subplot(121)
+    # plt.plot(xi, u[:, T])
+    # plt.xlabel(r'$\xi$')
+    # plt.ylabel('u')
+
+    # plt.subplot(122)
+    # plt.plot(derivative_estimate, u[:-1, T], 'b.')
+    # plt.xlabel(r'$\frac{du}{d\xi}$')
+    # plt.ylabel("u")
+
+    plt.suptitle(f"Travelling wave at t = {T*dt}")
+
+
+
+def c_plot(u1, u2):
+    # t_vec = np.linspace(1, int(t_max/dt)-1, 10)
+    t_vec = np.linspace(1000, 10000, 10)
+
+    # t_vec = np.linspace(0,, 10)
+    for t in t_vec:
+        plt.subplot(121)
+        plt.plot(xi, u1[:, int(t)])
+        # plt.subplot(122)
+        # plt.plot(xi, u2[:, int(t)])
+
+
+
+L = 100
+t_max = 500
+dt = 0.01
+rho = 0.5
+q_val = 8
+xi = np.arange(1, L + 1)
+u_star_val = find_steady_states(q_val, rho) #u1 = u_star_val[2], u2 = u_star_val[1]
+print(u_star_val)
+
+T = 1000
+u_b = run_dynamics(50, u_star_val[1], 1)    #t=1000, xi0= 20, 50, 50 u0 = u1, u2, u2*1.1
+
+# v_estimate = estimate_velocity(u_b, T) # only works if u is a travelling wave
+# print(v_estimate)
+
+derivative_estimate = np.diff(u_b[:, T])
+b_plot(u_b, derivative_estimate)
+
+# u_c1 = run_dynamics(50, u_star_val[2], 2)
+# u_c2 = run_dynamics(50, u_star_val[2]*3, 2)
+# c_plot(u_c1, u_c2)
+
+
+plt.show()
---
+++ problem2.py
@@ -0,0 +1,63 @@
+import numpy as np
+import sympy as sym
+from matplotlib import pyplot as plt
+
+
+# a, b, u, v = sym.symbols("a b u v")
+# sol = sym.solve([a - (b+1)*u + (u**2)*v, b*u - (u**2)*v], (u, v))
+# print(sol)
+def disc_lap(f, x, y):
+    L = len(f[:, 0])
+    return f[(x + 1) % L, y] + f[(x - 1) % L, y] + f[x, (y + 1) % L] + f[x, (y - 1) % L] - 4*f[x, y]
+    
+
+def fu(u_grid, v_grid, x, y, a, b, Du):
+    return a - (b+1)*u_grid[x,y] + (u_grid[x,y]**2)*v_grid[x,y] + Du*(disc_lap(u_grid,x,y))
+
+
+def fv(u_grid, v_grid, x, y, b, Dv):
+    return b*u_grid[x,y] - (u_grid[x,y]**2)*v_grid[x,y] + Dv*(disc_lap(v_grid,x,y))   
+
+
+def eom(u_grid, v_grid, x, y, dt, a, b, Du, Dv):
+    new_x = u_grid[x,y] + dt * fu(u_grid, v_grid, x, y, a, b, Du)
+    new_y = v_grid[x,y] + dt * fv(u_grid, v_grid, x, y, b, Dv)
+    return new_x, new_y
+    
+
+def main():
+    a = 3
+    b = 8
+    Du = 1
+    Dv_vec = [2.3, 3, 5, 9]
+    Dv = 2.3
+    L = 128
+    t_max = 100
+    dt = 0.01
+
+
+    # plt.figure()
+
+    u_grid = (1.1*a - 0.9*a) * np.random.rand(L, L) + 0.9*a
+    v_grid = (1.1*b/a - 0.9*b/a) * np.random.rand(L, L) + 0.9*b/a
+    
+    for iter in range(int(t_max/dt)):
+        # if iter % 1000 == 0:
+        #     plt.imshow(u_grid)
+        #     plt.title(f"{Dv=} at iteration: {iter}, t = {iter*dt}")
+        #     plt.show()
+        
+        for x in range(L):
+            for y in range(L):
+                updated_u, updated_v = eom(u_grid, v_grid, x, y, dt, a, b, Du, Dv)
+                u_grid[x,y] = updated_u
+                v_grid[x,y] = updated_v
+
+    plt.imshow(u_grid)
+    plt.title(f"{Dv=} at iteration: {iter}, t = {iter*dt}")
+    plt.show()            
+
+
+if __name__ == "__main__":
+    main()
+    
