3aadfec209dae5f1bd37868afe1f3c7e709cac3d
matssonalex <alexmatsson@gmail.com>
2022-02-25 18:51:41 +0100

    merge

--- problem3.py
+++ problem3.py
@@ -13,19 +13,11 @@ def circle(vector):
     plt.show()
 
 
-
-
-
-# def g(omega):
-#     gamma = 1
-#     return (gamma/np.pi)/(omega**2 + gamma**2)
-
-
-gamma = 1
+gamma = 0.5
 K_c = 2*gamma
 K = [K_c-1, K_c+0.01, K_c+1]
-K = K[0]
-N = 30
+K = K[2]
+N = 300
 theta = np.random.uniform(np.pi/2, -np.pi/2, size=N)
 omega = np.random.standard_cauchy(size=N)
 T = 100
@@ -34,11 +26,11 @@ r = np.zeros(int(T/dt))
 t = np.linspace(0, T, int(T/dt))
 
 for j in range(int(T/dt)):
+    r[j] = np.sum(np.cos(theta)) / N
     for i in range(N):
-        r[j] = np.sqrt(1 + np.sum(np.cos(theta - theta[i])))
         #r[j] = np.abs(np.sum(np.exp(theta)))
         theta[i] += dt*(omega[i] + K/N*np.sum(np.sin(theta - theta[i])))
 
 #circle(theta)
-plt.plot(t, r)
+plt.plot(t, np.abs(r))
 plt.show()
